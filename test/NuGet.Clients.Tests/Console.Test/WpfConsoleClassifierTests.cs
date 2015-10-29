ff4bdd24f6d225f79a94070202f05044193a1e23
deepakaravindr <daravind@microsoft.com>
2015-10-29 12:41:43 -0700

    Backup commit of Console.Test

---
+++ test/NuGet.Clients.Tests/Console.Test/WpfConsoleClassifierTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NuGetConsole.Implementation.Console;
+using Xunit;
+
+namespace Console.Test
+{
+    public class WpfConsoleClassifierTests
+    {
+        [Fact]
+        public void WpfConsoleClassifier_GetClassificationSpans_Fail()
+        {
+            // Arrange
+            var wpfConsoleService = new WpfConsoleService();
+        }
+    }
+}
